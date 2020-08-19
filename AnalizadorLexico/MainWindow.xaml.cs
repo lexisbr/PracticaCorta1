532bd3cbd02f09b2a5675f3210f260e865d02038
lexisbr <josebarriosrodas@gmail.com>
2020-08-18 21:48:56 -0600

    Clasifica tokens

--- AnalizadorLexico/MainWindow.xaml.cs
+++ AnalizadorLexico/MainWindow.xaml.cs
@@ -41,13 +41,16 @@ namespace AnalizadorLexico
         {
             char delimitador = ' ';
             int num = 0;
+            double num2 = 0.00;
             int o = 0;
+            String[] d = new String[2];
             String[] palabras = oracion.Split(delimitador);
-            for (int i = 0; i < palabras.Length; i++)
+            
+          /*  for (int i = 0; i < palabras.Length; i++)
             {
             System.Console.WriteLine(">>>"+palabras[i]);
 
-            }
+            }*/
 
             for (int i = 0; i < palabras.Length; i++)
             {
@@ -55,21 +58,39 @@ namespace AnalizadorLexico
                 {
                     System.Console.WriteLine(palabras[i] + " Es un digito");
                 }
+                else if(double.TryParse(palabras[i], out num2))
+                 {
+                    System.Console.WriteLine(palabras[i] + " Es un double");
+                }
+                d[0] = "";
+                d[1] = "";
                 foreach (char caracter in palabras[i])
                 {
 
-                    System.Console.WriteLine(caracter);
-                    if (Char.IsLetter(caracter))
+                    //System.Console.WriteLine(caracter);
+                    //System.Console.WriteLine(">>>"+caracter.Equals('Q'));
+
+                    if ((Char.IsLetter(caracter))&&(!caracter.Equals('Q')))
                     {
                         System.Console.WriteLine(palabras[i]+" Es una palabra");
                         break;
-                    }
-                    if(caracter.Equals("Q"))
+                    }else if(caracter.Equals('Q'))
                     {
-
+                        d[0] = "Q";
+                    }else if (caracter.Equals('.'))
+                    {
+                        if (d[0].Equals("Q"))
+                        {
+                            d[1] = ".";
+                        }
+                    }else if (Char.IsDigit(caracter))
+                    {
+                        if (d[0].Equals("Q") && d[1].Equals("."))
+                        {
+                            System.Console.WriteLine(palabras[i] + " Es dinero");
+                        }
                     }
 
-                    
                 }
             }
             
